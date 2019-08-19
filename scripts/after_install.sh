59224e2ff88d93cc0c1f5ae7bfbd2465647136f2
Ian Weyenberg <eeyonw@gmail.com>
2019-08-19 16:08:09 -0700

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd /var/www/html/spark
+
+php artisan down
+php artisan migrate --force
+php artisan cache:clear
+php artisan up
+
+touch ./storage/logs/laravel.log
+
+cp /root/configs/compltit.net/spark.env /var/www/html/spark/.env
+
+chmod -R ug+rwx ./storage ./bootstrap/cache
