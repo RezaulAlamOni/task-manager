725112ddcad6c04d8065d7367e5a82300f0558c5
Ian Weyenberg <eeyonw@gmail.com>
2019-08-19 18:00:17 -0700

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -11,3 +11,6 @@ touch ./storage/logs/laravel.log
 cp /root/configs/compltit.net/spark.env /var/www/html/spark/.env
 
 chmod -R ug+rwx ./storage ./bootstrap/cache
+
+cd vendor/laravel
+ln -s ../../spark/ spark-aurelius
