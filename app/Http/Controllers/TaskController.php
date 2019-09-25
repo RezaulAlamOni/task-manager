bd5c3b4bb65bf43413fa58b4f3f66b690feb9549
Shahinur Rahman <coredeveloper.2013@gmail.com>
2019-09-25 14:27:41 +0600

    Create new Component for task details and pass data here

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -59,7 +59,7 @@ class TaskController extends Controller
 //                'created_at' => Carbon::now(),
 //            ];
 //            $task = Task::create($data);
-//            $this->createLog($task->id, 'created', 'Create empty task', '');
+//            $this->createLog($task->id, 'created', 'Create task', '');
 //            $tasks = Task::where('parent_id', 0)
 //                ->where('project_id', $request->id)
 //                ->where('list_id', $list_id)
@@ -109,7 +109,7 @@ class TaskController extends Controller
                 'created_at' => Carbon::now(),
             ];
             $task = Task::create($data);
-            $this->createLog($task->id, 'created', 'Create empty task', $request->text);
+            $this->createLog($task->id, 'created', 'Create task', $request->text);
             return response()->json(['success' => $task]);
 
         }
@@ -150,7 +150,7 @@ class TaskController extends Controller
                 'created_at' => Carbon::now(),
             ];
             $task = Task::create($data);
-            $this->createLog($task->id, 'created', 'Create empty task', $request->text);
+            $this->createLog($task->id, 'created', 'Create task', $request->text);
             return response()->json(['success' => $task]);
 
         }
