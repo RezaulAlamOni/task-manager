eb635949ccaed4aa56fe66a4c17c72d712b9fca7
Shahinur Rahman <coredeveloper.2013@gmail.com>
2019-10-04 11:22:36 +0600

    update copy and past with tag update and copy text

--- app/Http/Controllers/AssignedUserController.php
+++ app/Http/Controllers/AssignedUserController.php
@@ -7,77 +7,41 @@ use Illuminate\Http\Request;
 
 class AssignedUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\AssignedUser  $assignedUser
-     * @return \Illuminate\Http\Response
-     */
     public function show(AssignedUser $assignedUser)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\AssignedUser  $assignedUser
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(AssignedUser $assignedUser)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\AssignedUser  $assignedUser
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, AssignedUser $assignedUser)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\AssignedUser  $assignedUser
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(AssignedUser $assignedUser)
     {
         //
