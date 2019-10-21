94744cfbe08d48bdaf46770243557b7a4d2a5fe8
Shahinur Rahman <coredeveloper.2013@gmail.com>
2019-10-21 16:24:30 +0600

    update tag table and add migration fro create table assign tag

---
+++ app/AssignTag.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AssignTag extends Model
+{
+    protected $fillable = [
+        'task_id',
+        'tag_id'
+    ];
+
+    public function task()
+    {
+        return $this->hasOne(Task::class, 'id', 'task_id');
+    }
+
+    public function tag()
+    {
+        return $this->hasOne(Tags::class, 'id', 'tag_id');
+    }
+
+}
