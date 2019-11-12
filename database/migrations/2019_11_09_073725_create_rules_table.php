3a2d2badcb84be608a7798bb5d3329ebcdf52506
Shahinur Rahman <coredeveloper.2013@gmail.com>
2019-11-12 11:04:02 +0600

    update rules migration

--- database/migrations/2019_11_09_073725_create_rules_table.php
+++ database/migrations/2019_11_09_073725_create_rules_table.php
@@ -18,10 +18,11 @@ class CreateRulesTable extends Migration
             $table->string('name');
             $table->tinyInteger('status')->default(0);
             $table->integer('project_id');
-            $table->integer('project_id');
+            $table->integer('board_id');
             $table->bigInteger('move_from');//column id (from task)
             $table->bigInteger('move_to');//column id (from task)
             $table->bigInteger('created_by');//user id (from task)
+            $table->string('assigned_users');//user assign (user)
             $table->timestamps();
         });
     }
