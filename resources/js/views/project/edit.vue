c66f92913b639eb9b19a7e8f7d76bfce7446247a
Shahinur Rahman <coredeveloper.2013@gmail.com>
2020-01-09 12:42:38 +0600

    update all project and add project page design

--- resources/js/views/project/edit.vue
+++ resources/js/views/project/edit.vue
@@ -1,44 +1,45 @@
 <template>
+    <div class="container">
+        <div class="col-md-12 ">
+            <div class="card card-default border-primary">
+                <div class="card-header" id="header-item" style="font-weight: bold">
+                    Create Project
+                </div>
+                <div class="card-body">
+                    <form role="form">
+                        <!-- Token Name -->
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Project Title</label>
 
-    <div class="col-md-6 offset-md-3">
-        <div class="card card-default border-primary">
-            <div class="card-header bg-primary text-white" id="header-item">
-                Create Project
+                            <div class="col-md-6">
+                                <input class="form-control" name="name" type="text" v-model="project.title">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Project Description</label>
+
+                            <div class="col-md-6">
+                                <input class="form-control" name="description" type="text" v-model="project.description">
+                            </div>
+                        </div>
+
+                        <!-- Create Button -->
+                        <div class="form-group row mb-0" style="text-align: right">
+                            <div class="offset-md-4 col-md-6">
+                                <button @click.prevent="addProject" class="btn btn-primary" type="submit"
+                                        v-if="id === undefined">Create
+                                </button>
+                                <button @click.prevent="UpdateProject" class="btn btn-primary" v-else type="submit">Update
+                                </button>
+                                <a class="btn btn-default" href="/projects">Back</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="card-body">
-                <form role="form">
-                    <!-- Token Name -->
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">Project Title</label>
 
-                        <div class="col-md-6">
-                            <input class="form-control" name="name" type="text" v-model="project.title">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">Project Description</label>
-
-                        <div class="col-md-6">
-                            <input class="form-control" name="description" type="text" v-model="project.description">
-                        </div>
-                    </div>
-
-                    <!-- Create Button -->
-                    <div class="form-group row mb-0">
-                        <div class="offset-md-4 col-md-6">
-                            <button @click.prevent="addProject" class="btn btn-primary" type="submit"
-                                    v-if="id === undefined">Create
-                            </button>
-                            <button @click.prevent="UpdateProject" class="btn btn-primary" v-else type="submit">Update
-                            </button>
-                            <a class="btn btn-default" href="/projects">Cancel</a>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            <!-- Show Token Modal -->
         </div>
-
-        <!-- Show Token Modal -->
     </div>
 
 </template>
@@ -48,7 +49,7 @@
         data() {
             return {
                 project: {
-                    id : null,
+                    id: null,
                     title: null,
                     description: null
                 },
@@ -83,7 +84,7 @@
             },
             FindProject() {
                 var _this = this;
-                axios.get('/api/project/'+_this.id)
+                axios.get('/api/project/' + _this.id)
                     .then(response => response.data)
                     .then(response => {
                         if (response.success == 1) {
@@ -101,7 +102,7 @@
             UpdateProject() {
                 var _this = this;
                 if (_this.project.title.length > 3) {
-                    axios.post('/api/project-update',_this.project)
+                    axios.post('/api/project-update', _this.project)
                         .then(response => response.data)
                         .then(response => {
                             if (response.status === 'success') {
--- resources/js/views/project/index.vue
+++ resources/js/views/project/index.vue
@@ -1,25 +1,21 @@
 <template>
     <aside class="right-aside">
         <div class="container">
-            <section>
-                <div class="container-header">
-                    <h2>
-                        <i class="fa fa-fw fa-tasks"></i> Projects
-                        <router-link :to="{ name: 'project-create'}">
-                            <button class="btn btn-primary" style="float: right;margin-right: 20px;" type="submit"> Create Project</button>
-                        </router-link>
-                    </h2>
-                </div>
-            </section>
             <section class="content">
                 <div class="row user-list">
                     <div class="col-lg-12">
                         <div class="card bg-primary-card">
+                            <div class="card-header">
+                                <h2>
+                                    <i class="fa fa-fw fa-tasks"></i> Projects
+                                    <router-link :to="{ name: 'project-create'}">
+                                        <button class="btn btn-primary" style="float: right;margin-right: 20px;" type="submit"> Create Project</button>
+                                    </router-link>
+                                </h2>
+                            </div>
                             <div class="card-body">
-                                <div class="table-responsive">
-
-
-                                    <div class="table-responsive" data-v-095ab3dc="">
+                                <div class="table-responsive" >
+                                    <div class="table-responsive" data-v-095ab3dc="" style="padding: 15px 10px;">
                                         <table class="table" data-v-095ab3dc="">
                                             <thead data-v-095ab3dc="">
                                             <tr data-v-095ab3dc="">
@@ -29,7 +25,7 @@
                                                     Description <i class="fa float-right" data-v-095ab3dc=""></i></th>
                                                 <th class="sortable" data-v-095ab3dc="" style="width: auto;">
                                                     Status <i class="fa float-right" data-v-095ab3dc=""></i></th>
-                                                <th class="sortable" data-v-095ab3dc="" style="width: auto;">
+                                                <th class="sortable" data-v-095ab3dc="" style="width: auto;text-align: right">
                                                     Actions <i class="fa float-right" data-v-095ab3dc=""></i></th>
                                             </tr>
                                             </thead>
@@ -40,7 +36,7 @@
                                                 </td>
                                                 <td v-text="project.description ? project.description : ''"></td>
                                                 <td>Active</td>
-                                                <td class="table-option">
+                                                <td class="table-option" style="text-align: right;">
                                                     <a @click.prevent="editProject(project)" class="compltit-blue-a badge badge-info"
                                                        href="javascript:void(0)"
                                                        title="edit_project">Edit</a>
@@ -52,34 +48,34 @@
                                             </tbody>
                                         </table>
                                     </div>
-                                    <div class="table-footer" data-v-095ab3dc="">
-                                        <div class="datatable-length float-left pl-3" data-v-095ab3dc=""><span
-                                            data-v-095ab3dc="">Rows per page:</span>
-                                            <select class="custom-select" data-v-095ab3dc="">
-                                                <option data-v-095ab3dc="" value="5">5</option>
-                                                <option data-v-095ab3dc="" value="10">10</option>
-                                                <option data-v-095ab3dc="" value="20">20</option>
-                                                <option data-v-095ab3dc="" value="50">50</option>
-                                                <option data-v-095ab3dc="" value="-1">All</option>
-                                            </select>
-                                            <div class="datatable-info  pb-2 mt-3" data-v-095ab3dc=""><span
-                                                data-v-095ab3dc="">Showing </span> 1 -10 of 20 <span
-                                                data-v-095ab3dc="">records</span>
-                                            </div>
-                                        </div>
-                                        <div class="float-right" data-v-095ab3dc="">
-                                            <div class="btn-group" data-v-095ab3dc="">
-                                                <a class="btn btn-default" data-v-095ab3dc="" href="javascript:void(0)"
-                                                   tabindex="0">
-                                                    <i class="fa fa-angle-left" data-v-095ab3dc=""></i>
-                                                </a>
-                                                <a class="btn btn-default" data-v-095ab3dc="" href="javascript:void(0)"
-                                                   tabindex="0">
-                                                    <i class="fa fa-angle-right" data-v-095ab3dc=""></i>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
+<!--                                    <div class="table-footer" data-v-095ab3dc="">-->
+<!--                                        <div class="datatable-length float-left pl-3" data-v-095ab3dc=""><span-->
+<!--                                            data-v-095ab3dc="">Rows per page:</span>-->
+<!--                                            <select class="custom-select" data-v-095ab3dc="">-->
+<!--                                                <option data-v-095ab3dc="" value="5">5</option>-->
+<!--                                                <option data-v-095ab3dc="" value="10">10</option>-->
+<!--                                                <option data-v-095ab3dc="" value="20">20</option>-->
+<!--                                                <option data-v-095ab3dc="" value="50">50</option>-->
+<!--                                                <option data-v-095ab3dc="" value="-1">All</option>-->
+<!--                                            </select>-->
+<!--                                            <div class="datatable-info  pb-2 mt-3" data-v-095ab3dc=""><span-->
+<!--                                                data-v-095ab3dc="">Showing </span> 1 -10 of 20 <span-->
+<!--                                                data-v-095ab3dc="">records</span>-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+<!--                                        <div class="float-right" data-v-095ab3dc="">-->
+<!--                                            <div class="btn-group" data-v-095ab3dc="">-->
+<!--                                                <a class="btn btn-default" data-v-095ab3dc="" href="javascript:void(0)"-->
+<!--                                                   tabindex="0">-->
+<!--                                                    <i class="fa fa-angle-left" data-v-095ab3dc=""></i>-->
+<!--                                                </a>-->
+<!--                                                <a class="btn btn-default" data-v-095ab3dc="" href="javascript:void(0)"-->
+<!--                                                   tabindex="0">-->
+<!--                                                    <i class="fa fa-angle-right" data-v-095ab3dc=""></i>-->
+<!--                                                </a>-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
                                 </div>
                             </div>
                         </div>
