062fd87ab8844cd90b9b83a8aa11e7eca17a1467
Ferdous Anam <ferdous.anam@gmail.com>
2020-01-09 11:59:27 +0600

    Customized Design

--- resources/js/views/notification/add.vue
+++ resources/js/views/notification/add.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="col-md-6 offset-md-3">
         <div class="card card-default border-primary">
-            <div id="header-item" class="card-header bg-primary text-white">Project Create</div>
+            <div id="header-item" class="card-header">
+                <b><i class="fa fa-bell-o"/> Add or Remove Email & Notifications</b>
+            </div>
             <div class="card-body">
                 <form role="form">
-                    <div class="form-group row"><label class="col-md-4 col-form-label text-md-right">Project
-                        Title</label>
-                        <div class="col-md-6"><input name="name" type="text" class="form-control"></div>
-                    </div>
-                    <div class="form-group row"><label class="col-md-4 col-form-label text-md-right">Project
-                        Description</label>
-                        <div class="col-md-6"><input name="description" type="text" class="form-control"></div>
+                    <div class="form-group row">
+                        <label class="col-md-4 col-form-label text-md-right">
+                            Project Title
+                        </label>
+                        <div class="col-md-6">
+                            <input name="name" type="text" class="form-control">
+                        </div>
                     </div>
                     <div class="form-group row mb-0">
                         <div class="offset-md-4 col-md-6">
-                            <button type="submit" class="btn btn-primary">Create
+                            <button type="button" class="btn btn-primary">
+                                Create
                             </button>
-                            <a href="/projects" class="btn btn-default">Cancel</a></div>
+                            <router-link :to="{ name: 'NotificationSettings' }" class="btn btn-secondary">Cancel</router-link>
+                        </div>
                     </div>
                 </form>
             </div>
