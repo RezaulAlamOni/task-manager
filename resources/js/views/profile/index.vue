b1f6ed04354163901561f0e4f3f1c1e98f38c3ab
Ferdous Anam <ferdous.anam@gmail.com>
2020-01-10 12:13:27 +0600

    Design Change About Section

--- resources/js/views/profile/index.vue
+++ resources/js/views/profile/index.vue
@@ -1,142 +1,124 @@
 <template>
-    <aside class="right-aside">
-        <div class="container">
-            <section>
-                <div class="container-header">
-                    <h2>
-                        <i class="fa fa-user"></i> Profile
-                    </h2>
+  <div style="height: calc(100vh - 77px);overflow: auto;}">
+    <div class="container">
+      <section class="content">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card bg-primary-card">
+              <div id="header-item" class="card-header">
+                <div class="row">
+                  <div class="col text-left">
+                    <b>
+                      <i class="fa fa-user"></i> Profile
+                    </b>
+                  </div>
+                  <div class="col mr-4 text-right" style="display: none;">
+                    <router-link :to="{ name: 'ProfileEdit' }" class="profile-edit-btn">Edit Profile</router-link>
+                  </div>
                 </div>
-            </section>
-            <section class="content">
-                <div class="row user-list">
-                    <div class="col-lg-12">
-                        <div class="card bg-primary-card">
-                            <div class="card-body">
-                                <div class="container emp-profile">
-                                    <form method="post">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <div class="profile-img">
-                                                    <img :src="profile.photo_url" alt="Avatar"/>
-                                                    <div class="file btn btn-lg btn-primary">
-                                                        <input type="file" name="file"/>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6 border pl-0 pr-0">
-                                                <div class="profile-head">
-                                                    <h1 class="card-header bg-secondary text-white">{{ profile.name }}</h1>
-                                                    <h6></h6>
-                                                    <p class="proile-rating"><span></span></p>
-                                                    <ul class="nav nav-tabs ml-2 mt-10" id="myTab" role="tablist">
-                                                        <li class="nav-item">
-                                                            <a class="nav-link active" id="home-tab" data-toggle="tab"
-                                                               href="#home" role="tab" aria-controls="home"
-                                                               aria-selected="true">About</a>
-                                                        </li>
-                                                        <li class="nav-item">
-                                                            <a class="nav-link" id="profile-tab" data-toggle="tab"
-                                                               href="#profile" role="tab" aria-controls="profile"
-                                                               aria-selected="false">Timeline</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="row ml-2">
-                                                    <div class="col-md-12">
-                                                        <div class="tab-content profile-tab" id="myTabContent">
-                                                            <div class="tab-pane fade show active" id="home"
-                                                                 role="tabpanel"
-                                                                 aria-labelledby="home-tab">
-                                                                <div class="row">
-                                                                    <div class="col-md-4">
-                                                                        Name
-                                                                    </div>
-                                                                    <div class="col-md-8">
-                                                                        <p>{{ profile.name }}</p>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col-md-4">
-                                                                        Email
-                                                                    </div>
-                                                                    <div class="col-md-8">
-                                                                        <div class="row pr-0">
-                                                                            <div class="col-md-6">
-                                                                                <p>{{ profile.email }}</p>
-                                                                            </div>
-                                                                            <div class="col-md-6 pr-0" v-if="profile.email_verified_at === null">
-                                                                                <a class="profile-edit-btn">
-                                                                                    Verify  Email
-                                                                                </a>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                                                <div class="row">
-                                                                    <div class="col-md-4">
-                                                                        <label>Experience</label>
-                                                                    </div>
-                                                                    <div class="col-md-8">
-                                                                        <p>Expert</p>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-2 mt-3" style="display: none">
-                                                <router-link :to="{ name: 'ProfileEdit' }" class="profile-edit-btn">Edit Profile</router-link>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <div class="profile-work">
-                                                    <p>WORK LINK</p>
-                                                    <a href="#">Website Link</a><br/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
+              </div>
+              <div class="card-body">
+                <div class="container emp-profile">
+                  <div class="row">
+                    <div class="col-4 pl-0">
+                      <div class="profile-img">
+                        <img :src="profile.photo_url" alt="Avatar" />
+                        <div class="file btn btn-lg btn-primary">
+                          <input type="file" name="file" />
                         </div>
+                      </div>
                     </div>
+                    <div class="col-8 border pl-0 pr-0">
+                      <div class="profile-head">
+                        <h1 class="card-header bg-secondary text-white">{{ profile.name }}</h1>
+                        <h6></h6>
+                        <p class="proile-rating">
+                          <span></span>
+                        </p>
+                      </div>
+                      <div class="row ml-2">
+                        <div class="col-md-12">
+                          <div class="row">
+                            <div class="col-md-2 font-weight-bold">Name</div>
+                            <div class="col-md-10">
+                              <p>{{ profile.name }}</p>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-md-2 font-weight-bold">Email</div>
+                            <div class="col-md-10">
+                              <div class="row pr-0">
+                                <div class="col-md-6">
+                                  <p>{{ profile.email }}</p>
+                                </div>
+                                <div
+                                  class="col-md-6 pr-0"
+                                  v-if="profile.email_verified_at === null"
+                                >
+                                  <a class="profile-edit-btn">Verify Email</a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="card">
+                        <div class="card-header bg-secondary text-white">About</div>
+                        <div class="card-body">
+                          <div class="profile-work">
+                            <p>WORK LINK</p>
+                            <a href="#">Website Link</a>
+                            <br />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </section>
+              </div>
+            </div>
+          </div>
         </div>
-    </aside>
+      </section>
+    </div>
+  </div>
 </template>
 <script>
-    export default {
-        components: {},
-        data() {
-            return {
-                profile: {},
-            };
-        },
-        created() {
-            this.getUser();
-        },
-        mounted() {
-        },
-        methods: {
-            getUser() {
-                let _this = this;
-                axios.get('/api/auth-user')
-                    .then(response => response.data)
-                    .then(response => {
-                        _this.profile = response.user;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            // return response()->json(Schema::getColumnListing('task_lists'));
-        },
-        watch: {}
+export default {
+  components: {},
+  data() {
+    return {
+      profile: {}
+    };
+  },
+  created() {
+    this.getUser();
+  },
+  mounted() {},
+  methods: {
+    getUser() {
+      let _this = this;
+      axios
+        .get("/api/auth-user")
+        .then(response => response.data)
+        .then(response => {
+          _this.profile = response.user;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
+    // return response()->json(Schema::getColumnListing('task_lists'));
+  },
+  watch: {}
+};
 </script>
+<style>
+.card-header b {
+  font-size: 25px;
+}
+</style>
