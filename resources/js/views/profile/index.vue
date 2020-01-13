ea472e57c4d3287d01ecfc6c21eb7cd1a92cc3c9
Ferdous Anam <ferdous.anam@gmail.com>
2020-01-13 14:53:53 +0600

    Update button tooltip

--- resources/js/views/profile/index.vue
+++ resources/js/views/profile/index.vue
@@ -49,7 +49,12 @@
                             data-target="#personal_info"
                             @click="getProfile"
                           >
-                            <i class="fal fa-edit" aria-hidden="true"></i>
+                            <i
+                              class="fal fa-edit"
+                              aria-hidden="true"
+                              data-toggle="tooltip"
+                              :data-original-title="profileUpdatedAt(profile.updated_at)"
+                            ></i>
                           </a>
                         </div>
                         <div class="card-body">
@@ -96,7 +101,12 @@
                             data-target="#card_info"
                             @click="getCardInfo"
                           >
-                            <i class="fal fa-edit" aria-hidden="true"></i>
+                            <i
+                              class="fal fa-edit"
+                              aria-hidden="true"
+                              data-toggle="tooltip"
+                              :data-original-title="profileUpdatedAt(profile.updated_at)"
+                            ></i>
                           </a>
                         </div>
                         <div class="card-body">
@@ -133,7 +143,12 @@
                             data-target="#billing_info"
                             @click="getBillingInfo"
                           >
-                            <i class="fal fa-edit" aria-hidden="true"></i>
+                            <i
+                              class="fal fa-edit"
+                              aria-hidden="true"
+                              data-toggle="tooltip"
+                              :data-original-title="profileUpdatedAt(profile.updated_at)"
+                            ></i>
                           </a>
                         </div>
                         <div class="card-body">
@@ -221,10 +236,22 @@ export default {
     };
   },
   created() {
+    // moment.tz.setDefault("America/New_York");
     this.getUser();
   },
   mounted() {},
   methods: {
+    profileUpdatedAt(value) {
+      console.log(moment.utc());
+      return (
+        "Profile Updated " +
+        moment
+          .utc(value)
+          .local()
+          .locale("en-short")
+          .fromNow()
+      );
+    },
     getUser() {
       let _this = this;
       axios
