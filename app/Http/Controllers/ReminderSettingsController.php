bd5b0319a96049623536b9ff90d64e5fb4edef0f
Ferdous Anam <ferdous.anam@gmail.com>
2020-01-14 14:57:11 +0600

    Send Reminder Email Data filtered

--- app/Http/Controllers/ReminderSettingsController.php
+++ app/Http/Controllers/ReminderSettingsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReminderSettingsController extends Controller
@@ -11,6 +13,31 @@ class ReminderSettingsController extends Controller
      */
     public function sendDueDateEmail()
     {
-        echo "Reminder Sending Logic will be implemented Here";
+        $user = User::whereHas('tasks', function ($q) {
+            $q->whereDate('date', Carbon::today()->addDays(2)->format("Y-m-d"));
+        })->whereHas('notifications', function ($q) {
+            $q->where('unique_id', 'reminder_whenTaskIsDue');
+        })->with(['tasks' => function ($q) {
+            $q->whereDate('date', Carbon::today()->addDays(2)->format("Y-m-d"));
+        }, 'notifications' => function ($qr) {
+            $qr->where('unique_id', 'reminder_whenTaskIsDue');
+        }])->get();
+        return $user;
+    }
+
+    /**
+     * @param \DateTime $date
+     * @param int $sendBefore
+     * @return bool
+     */
+    private function isTimeToSend($date, $sendBefore = 0)
+    {
+        $today = Carbon::today()->addDays($sendBefore)->format("Y-m-d");
+        $sendingDate = Carbon::parse($date)->format("Y-m-d");
+        if ($today == $sendingDate) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
